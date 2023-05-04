29a8cda50938dfc5c48db6b62f88fb79e3604a4a
Shoxrux Code <shoxruxpl04@gmail.com>
2023-05-05 00:39:58 +0500

    added dynamic routes

--- modules/product.js
+++ modules/product.js
@@ -7,67 +7,69 @@ header()
 
 
 axios.get("http://localhost:3000/goods")
-.then(res => product_reload(res.data , productCont))
-
-
+.then(res => product_reload(res.data, productCont))
+const url = location
+const id = url.href.split('=').at(-1)
+console.log(id);
 
 function product_reload(arr, place) {
     place.innerHTML = "";
-    
     for (let item of arr) {
-        place.innerHTML += `
-        <div class="topOfProduct">
-    <div class="left-scroll">
-        <img src="${item.media}" class="swiperImg" alt="">
-        </div>
-        <div class="sliderProduct">
-        <img src="/public/arrow-left.svg" class="arrowProduct-left" alt="">
-        <div class="productImgs">
-        <img src="${item.media}" class="carouselImg" alt="">
-        </div>
-        <img src="/public/arrow-right.svg" class="arrowProduct-right" alt="">
-        </div>
-        <div class="info">
-        <h2 class="titlePR">${item.title}</h2>
-        <div class="prices">
-        <span class="newPrice">${item.price}</span>
-        <span class="discountPrice">45 000 сум</span>
-        </div>
-        <div class="counter">
-        <span class="minus">-</span>
-        <span class="numberCount">1</span>
-        <span class="plus">+</span>
-        </div>
-        <div class="hr"></div>
-        <p>Станьте востребованным разработчиком. Вы изучите основы программирования и основные концепции
-        компьютерных наук, цифровые технологии, операционные системы, программное обеспечение, базы данных,
-        системы аналитики, языки программирования и многое другое. Познакомитесь с тестированием и системным
-        анализом. На программе сможете сделать осознанный выбор специализации и технологий, прокачаться в
-        выбранном направлении.
-        </p>
-        <div class="btns">
-        <button class="addBasket">Добавить в корзину</button>
-        <button class="addSaved">Добавить в избранное</button>
-        </div>
-        </div>
-        </div>
-        <div class="bottomOfProduct">
-        <div class="descProduct">
-        <h2>Описание товара</h2>
-        <p>
-        Стиральный порошок Tide Lenor Touch подходит для использования в стиральных машинах любого типа.
-        Сочетание очищающего эффекта порошка и смягчающего эффекта Lenor делает вещи безупречно чистыми,
-        нежными и свежими. Стирайте согласно рекомендациям на ярлыках текстильных изделий. Рекомендованный
-            режим стирки — от 20 минут при температуре воды 30'C и выше. Обратите внимание: порошок не
-            предназначен для стирки изделий из шерсти и шелка. Синтетическое моющее средство порошкообразное для
-            использования в стиральных машинах любого типа. Рекомендованная температура стирки указана на
-            ярлыках текстильных изделий. Не перегружайте стиральную машину. Оставьте зазор в ширину ладони между
-            загруженным бельем и барабаном. Белье будет двигаться свободно, стирка будет более эффективной.
-            </p>
+        if(item.id === +id) {
+            return place.innerHTML += `
+            <div class="topOfProduct">
+        <div class="left-scroll">
+            <img src="${item.media}" class="swiperImg" alt="">
             </div>
-            <div class="carousel"></div>
-            </div>    
-            `;
+            <div class="sliderProduct">
+            <img src="/public/arrow-left.svg" class="arrowProduct-left" alt="">
+            <div class="productImgs">
+            <img src="${item.media}" class="carouselImg" alt="">
+            </div>
+            <img src="/public/arrow-right.svg" class="arrowProduct-right" alt="">
+            </div>
+            <div class="info">
+            <h2 class="titlePR">${item.title}</h2>
+            <div class="prices">
+            <span class="newPrice">${item.price}</span>
+            <span class="discountPrice">45 000 сум</span>
+            </div>
+            <div class="counter">
+            <span class="minus">-</span>
+            <span class="numberCount">1</span>
+            <span class="plus">+</span>
+            </div>
+            <div class="hr"></div>
+            <p>Станьте востребованным разработчиком. Вы изучите основы программирования и основные концепции
+            компьютерных наук, цифровые технологии, операционные системы, программное обеспечение, базы данных,
+            системы аналитики, языки программирования и многое другое. Познакомитесь с тестированием и системным
+            анализом. На программе сможете сделать осознанный выбор специализации и технологий, прокачаться в
+            выбранном направлении.
+            </p>
+            <div class="btns">
+            <button class="addBasket">Добавить в корзину</button>
+            <button class="addSaved">Добавить в избранное</button>
+            </div>
+            </div>
+            </div>
+            <div class="bottomOfProduct">
+            <div class="descProduct">
+            <h2>Описание товара</h2>
+            <p>
+            Стиральный порошок Tide Lenor Touch подходит для использования в стиральных машинах любого типа.
+            Сочетание очищающего эффекта порошка и смягчающего эффекта Lenor делает вещи безупречно чистыми,
+            нежными и свежими. Стирайте согласно рекомендациям на ярлыках текстильных изделий. Рекомендованный
+                режим стирки — от 20 минут при температуре воды 30'C и выше. Обратите внимание: порошок не
+                предназначен для стирки изделий из шерсти и шелка. Синтетическое моющее средство порошкообразное для
+                использования в стиральных машинах любого типа. Рекомендованная температура стирки указана на
+                ярлыках текстильных изделий. Не перегружайте стиральную машину. Оставьте зазор в ширину ладони между
+                загруженным бельем и барабаном. Белье будет двигаться свободно, стирка будет более эффективной.
+                </p>
+                </div>
+                <div class="carousel"></div>
+                </div>    
+                `;
+        }
         }
         
         //   const boxes = document.querySelectorAll('.box')
